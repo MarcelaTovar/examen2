b018d26a6fd865ba44a33c5f6864904bee8ba418
Marcela Tovar <marcelatovar.flores@gmail.com>
2024-06-18 20:59:22 -0600

    ruta de crear usuario

--- index.js
+++ index.js
@@ -3,45 +3,65 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 
-
-// Import the functions you need from the SDKs you need
 const { initializeApp } = require("firebase/app");
 
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDpWoLxzEwA93vHht5InD3aLpFyTw6Nd84",
-  authDomain: "examen-a9633.firebaseapp.com",
-  projectId: "examen-a9633",
-  storageBucket: "examen-a9633.appspot.com",
-  messagingSenderId: "434903720542",
-  appId: "1:434903720542:web:77f01dd17e531051724acf"
+    apiKey: "AIzaSyDpWoLxzEwA93vHht5InD3aLpFyTw6Nd84",
+    authDomain: "examen-a9633.firebaseapp.com",
+    projectId: "examen-a9633",
+    storageBucket: "examen-a9633.appspot.com",
+    messagingSenderId: "434903720542",
+    appId: "1:434903720542:web:77f01dd17e531051724acf"
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-const {getAuth,createUserWithEmailAndPassword} = require('firebase/auth')
+const { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut } = require('firebase/auth')
 
 
 // Middleware para parsear JSON
 app.use(express.json());
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Ruta de prueba
+// Ruta para crear usuario, la ruta recibe el email y la password del usuario
 app.post('/createUser', async (req, res) => {
-  const auth = getAuth(firebaseApp);
-  const email = req.body.email;
-  const password = req.body.password;
-  try {
-    const userCredentials = await createUserWithEmailAndPassword(auth,email,password);
-    res.status(200).send('Usuario creado con exito');
-  } catch (error) {
-    res.status(500).send('Usuario no creado con exito');
-  }
+    const auth = getAuth(firebaseApp);
+    const email = req.body.email;
+    const password = req.body.password;
+    try {
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+        res.status(200).send('Usuario creado con exito' + email);
+    } catch (error) {
+        res.status(500).send('Usuario no creado con exito');
+    }
 });
+
+app.post('/logIn', async (req, res) => {
+    const auth = getAuth(firebaseApp);
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+        const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+        res.status(200).send("Sesión inciada con éxito");
+    } catch (err) {
+        res.status(500).send('Error al iniciar sesion: ' + err.message);
+    }
+});
+
+app.post('/logOut', async (req, res) => {
+    const auth = getAuth(firebaseApp);
+    try {
+        await signOut(auth);
+        res.status(200).send("Sesión cerrada con éxito");
+    } catch (err) {
+        res.status(500).send('Error al cerrar sesión: ' + err.message);
+    }
+});
+
 
 // Iniciar el servidor
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
